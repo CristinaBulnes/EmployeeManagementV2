545b5b8a298547d7f0764d281074a9f9c2adf3a0
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-26 15:48:44 +0100

    Centralize app by controller and param from url

---
+++ advancedExample-MVC/libs/app.php
@@ -0,0 +1,19 @@
+<?php
+//class to centralize all the app
+class App {
+
+    function __construct () {
+        echo "<p>Nueva app</p>";
+        //obtain the url param that always is going to be when a param is in a url (becouse it was defined like this on htaccess)
+        $url = $_GET['url'];
+
+        //break down the url to get the first part as controller referent and the second part as method to execute
+            //Delete / character could be in the end of the url
+            $url = rtrim($url, '/');
+            //Break down each part of the url that are between / and save they into an array
+            $url = explode ('/', $url);
+            var_dump($url);
+
+    }
+}
+?>
