63472c2e205d303f8b2e0efa6263f7b8060538a2
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-26 18:08:38 +0100

    Render a view associate with a controller

--- advancedExample-MVC/libs/view.php
+++ advancedExample-MVC/libs/view.php
@@ -4,5 +4,9 @@ class View {
     function __construct () {
         echo "<p>Vista base</p>";
     }
+
+    function render ($nombre) { //$nombre: view you want to show
+        require 'views/'.$nombre.'.php';
+    }
 }
 ?>
