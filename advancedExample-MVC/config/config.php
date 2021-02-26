d365dee207419a74a962d057a4669ec8624263d1
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-26 21:43:03 +0100

    Define constants and configuration

---
+++ advancedExample-MVC/config/config.php
@@ -0,0 +1,11 @@
+<?php
+//Define constant and configuration for make future modification
+//make url static
+define ('URL', 'http://localhost/employeeManagement2/EmployeeManagementV2/advancedExample-MVC/');
+//reference database
+define ('HOST', 'localhost');
+define ('BD', '');
+define ('USER', 'root');
+define ('PASSWORD', );
+define ('CHARSET', 'utf8mb4');
+?>
