a6581d67ea2098edcd266676732430e4eae2f9f7
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-26 21:12:28 +0100

    Redirect to main also if url is empty

--- advancedExample-MVC/libs/app.php
+++ advancedExample-MVC/libs/app.php
@@ -4,9 +4,9 @@
 class App {
 
     function __construct () {
-        echo "<p>Nueva app</p>";
+        //echo "<p>Nueva app</p>";
         //obtain the url param that always is going to be when a param is in a url (becouse it was defined like this on htaccess)
-        $url = $_GET['url'];
+        $url = isset($_GET['url']) ? $_GET['url'] : null; // Check if url exits -> if not you have to charge index
 
         //break down the url to get the first part as controller referent and the second part as method to execute
             //Delete / character could be in the end of the url
@@ -15,7 +15,14 @@ class App {
             $url = explode ('/', $url);
             //var_dump($url);
 
-        //Charge controller read at url
+        //Charge controller read at url or index if url is null
+        if (empty($url[0])) {
+            $archivoController = 'controllers/main.php';
+            require_once $archivoController;
+            $controller = new Main ();
+            return false;
+        }
+
         $archivoController = 'controllers/' . $url[0] . '.php';
         //validate archive exits
         if (file_exists($archivoController)) {
