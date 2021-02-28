ca43ac0f56112f0ccf58f8b62b6ad202e83b31b2
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-28 09:50:45 +0100

    Show the information in the view

--- advancedExample-MVC/views/consulta/index.php
+++ advancedExample-MVC/views/consulta/index.php
@@ -6,11 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body class="container">
     <?php require 'views/header.php'; ?>
         <h2>Seccion de consulta</h2>
-        
-        <?php var_dump($this->datos);?>
+        <table class="table table-light">
+        <thead>
+            <tr>
+                <th scope="col">Matricula</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach ($this->datos as $row) {
+                echo "<tr>
+                        <td>{$row->matricula}</th>
+                        <td>{$row->nombre}</td>
+                        <td>{$row->apellido}</td>
+                        <td><a href=>Editar</a></td>
+                        <td><a href=>Eliminar</a></td>
+                    </tr>";
+            }
+            ?>
+            </tbody>
+        </table>
     <?php require 'views/footer.php'; ?>
 </body>
 </html>
